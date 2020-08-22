bc417dc9ccc35255bffa2c1fca87e5c3e07f0e2d
basicpail <azazln32@naver.com>
2020-08-22 09:53:39 +0900

    DoorLock 완성!!

--- DoorLock/DoorLock/main.c
+++ DoorLock/DoorLock/main.c
@@ -8,23 +8,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "Lcd.h"
-#define Keypadin PIND
 #include <string.h>
 
-int key=1;
-int pw_length = 6;
-int cmp;
+#define Keypadin PIND
+#define ModeSW PINB
 
-int m_cnt = 0 ;
+int key=1;
+int pw_length=6;
+int cmp;
+int Terminal=0;
+int Putch_int=0;
+
+int m_cnt=0;
 unsigned char input_start = 1;
 unsigned char old_input_start;
 
-Byte password[6]="111111";
-Byte Ispassword[6]={0,};
+
+Byte password[]="1111";
+Byte Ispassword[];
 Byte blank[6];
 
 int arr_length=0;
-unsigned char RX_data[16];
 
 Byte KeyPad()
 {
@@ -62,17 +66,29 @@ Byte KeyPad()
 	if((Keypadin & 0xF0) == 0xE0) {while((Keypadin & 0xF0)==0xE0);key=0; return '3';}
 	if((Keypadin & 0xF0) == 0xD0) {while((Keypadin & 0xF0)==0xD0);key=0; return '6';}
 	if((Keypadin & 0xF0) == 0xB0) {while((Keypadin & 0xF0)==0xB0);key=0; return '9';}
-	if((Keypadin & 0xF0) == 0x70) {while((Keypadin & 0xF0)==0x70);key=0; return '#';}
+	if((Keypadin & 0xF0) == 0x70)
+	{
+		while((Keypadin & 0xF0)==0x70);
+		return 0;
+	}
 	
 	return 0;
 }
 
 void putch(unsigned char *data)
 {
-	static int i=0;
+	//static int i=0;
 	while((UCSR0A & 0x20) == 0);
-	unsigned char data2 = *(data+(i++));
+	unsigned char data2 = *(data+(Putch_int++));
 	UDR0 = data2;
+	UCSR0A |= 0x20;
+}
+
+void _putch(unsigned char data) //putch_str에 쓰기 위함
+{
+	while((UCSR0A & 0x20) == 0);
+	
+	UDR0 = data;
 	UCSR0A |= 0x20;
 }
 
@@ -82,18 +98,33 @@ unsigned char getch()
 	//while((UCSR0A & 0x80)==0);
 	data = UDR0;
 	UCSR0A |= 0x80;
-	arr_length++;
+	//arr_length++;
 	return data;
+}
+
+void putch_str(char *str)
+{
+	unsigned char i = 0;
+	while(str[i] != '\0')
+	{
+		_putch(str[i++]);
+	}
 }
 
 int main(void)
 {
 	DDRD = 0x0F; //keypad
-	DDRA = 0xFF; //lcd
+	DDRA = 0xFF; //Lcd
+	
+	DDRB = 0xF6; //1111 0110 터미널
 
 	LcdInit_4bit();
 	
 	Byte str[] ="Put your PW!";
+	Byte str2[] ="\r\n****Terminal Mode****\r\nEnter your PW and press the 'x'\r\nTo change the PW, Press 'c'\r\n\0";
+	Byte str3[] ="\r\n****KeyPad Mode****\r\n\0";
+	Byte str4[] ="PW Change Mode";
+	Byte str5[] ="Change Completed";
 	
 	//시리얼
 	DDRE = 0xfe;
@@ -118,45 +149,127 @@ int main(void)
 		Lcd_STR(str);
 		Lcd_Pos(1,0);
 		
-		Ispassword[arr_length] = KeyPad(); //arr_length,arr_cnt
-		
-		if(key==0 && old_input_start==0)
+		if((ModeSW & 0xFF) == 0x08)  //0000 1000 터미널 모드
 		{
-			_delay_ms(10);
-			Put_into_arr();
-		}
-		
-		if(!((UCSR0A & 0x80)==0))
-		{
-			Ispassword[arr_length] = getch();
-			putch(Ispassword);
-			Lcd_STR(Ispassword);
-		}
-		
-		if(key==1 && old_input_start==1 )
-		{
-			_delay_ms(10);
-			
-			PORTC = 0xFE;
+			while((ModeSW & 0xFF)==0x08);
+			PORTC = 0xFE; //디버깅용
 			_delay_ms(100);
-			PORTC = 0xFF; //디버깅
+			PORTC = 0xFF;
+			PW_Clear();
+			putch_str(str2);
+			Terminal = 1;
+		}
+		
+		if((ModeSW & 0xFF) == 0x01)  //0000 0001 키패드로 복귀
+		{
+			while((ModeSW & 0xFF)==0x01);
+			PORTC = 0xFE; //디버깅용
+			_delay_ms(100);
+			PORTC = 0xFF;
+			PW_Clear();
+			putch_str(str3);
 			
-			if(CmpPW()==3)
+			Terminal = 0;
+		}
+		
+		if(Terminal ==0)
+		{
+			Ispassword[arr_length] = KeyPad();
+			
+			if(key==0 && old_input_start==0)
 			{
-				M_start();
-				old_input_start=0;
+				_delay_ms(10);
+				Put_into_arr();
+			}
+			
+			if(key==1 && old_input_start==1 )
+			{
+				if(!ComparePassword())
+				{
+					M_start();
+					old_input_start=0;
+				}
+				else
+				{
+					Incorrect();
+					old_input_start=0;
+				}
+			}
+		}
+		
+		if(Terminal == 1)
+		{
+			if(!((UCSR0A & 0x80)==0))
+			{
+				Ispassword[arr_length] = getch();
+				arr_length++;
+				putch(Ispassword);
+				Lcd_STR(Ispassword);
+				if(Ispassword[arr_length-1]=='x')
+				{
+					Ispassword[arr_length-1] = NULL; //strcmp 쓰기 위함
+					
+					if(!ComparePassword())
+					{
+						putch_str("\r\n");
+						putch_str("Door is Open!");
+						putch_str("\r\n");
+						M_start();
+						
+						Putch_int = 0;
+					}
+					else
+					{
+						putch_str("\r\n");
+						putch_str("Wrong PassWord!");
+						putch_str("\r\n");
+						Incorrect();
+						
+						Putch_int = 0;
+					}
+				}
+				
+				if(Ispassword[arr_length-1]=='c')
+				{
+					for (int x=0;x<4;x++)
+					{
+						password[x] = Ispassword[x];
+					}
+					password[arr_length-1] = NULL;
+					Putch_int = 0;
+					putch_str("\r\n");
+					putch_str("Change Complete!");
+					putch_str("\r\n");
+					PW_Clear();
+					Lcd_Clear();
+					Lcd_STR(str5);
+					_delay_ms(2000);
+					Lcd_Clear();
+					arr_length=0;
+				}
+				
 			}
 		}
 	}
 }
 
+
 void M_start(void)
 {
-	PORTC = 0xFE;
+	
+	Lcd_Clear();
+	Lcd_Pos(0,0);
+	Lcd_STR("Correct!");
+	Lcd_Pos(1,0);
 	Lcd_STR("Door is Open!!");
-	_delay_ms(3000);
+	
+	PORTC = 0xFE; // 1111 1110
+	_delay_ms(1000);
+	PORTC = 0x7F; // 0111 1111
+	_delay_ms(1000);
 	PORTC = 0xFF;
 	Lcd_Clear();
+	PW_Clear();
 	m_cnt=0;
 	arr_length=0;
 }
@@ -168,22 +281,6 @@ void Put_into_arr(void)
 	_delay_ms(100);
 	PORTC = 0xFF;
 	
-	if(arr_length==pw_length) // 지정된 암호길이
-	{
-		for (int x=0;x<pw_length;x++)
-		{
-			if(password[x]==Ispassword[x]){m_cnt++;}
-		}
-	}
-
-	if(arr_length>=10) //10은 임의의 길이
-	{
-		for(int i=0;i<10;i++)
-		{
-			Ispassword[i]="";
-		}
-		arr_length=0;
-	}
 	Lcd_STR(Ispassword);
 	key=1;
 }
@@ -191,33 +288,27 @@ void Put_into_arr(void)
 int ComparePassword()
 {
 	cmp = strcmp(Ispassword,password);
-	if(cmp==1)
-	{
-		PORTC = 0xFE; //디버깅용
-		_delay_ms(1000);
-		PORTC = 0xFF;
-	}
-	else if (cmp==-1)
-	{
-		PORTC = 0xFE; //디버깅용
-		_delay_ms(2000);
-		PORTC = 0xFF;
-	}
+	
 	return cmp;
 }
-
-int CmpPW()
+void Incorrect()
 {
-	for (int x=0;x<pw_length;x++)
+	Lcd_Clear();
+	Lcd_Pos(0,0);
+	Lcd_STR("Wrong PW!");
+	Lcd_Pos(1,0);
+	Lcd_STR("Try Again!!");
+	_delay_ms(2000);
+	Lcd_Clear();
+	PW_Clear();
+	m_cnt=0;
+	arr_length=0;
+}
+
+void PW_Clear()
+{
+	for(int x=0;x<16;x++)
 	{
-		if(password[x]==Ispassword[x]){m_cnt++;}
-	}
-	if(m_cnt==pw_length)
-	{
-		for (int x=0;x<pw_length;x++)
-		{
-			Ispassword[x]="";
-		}
-		return 3;
+		Ispassword[x]=NULL;
 	}
 }
